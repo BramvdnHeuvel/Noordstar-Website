a45ea35c309a6d19f93983a977abc9b27ee8c181
BramvdnHeuvel <git@bram.blmgroep.nl>
2020-10-20 23:07:02 +0200

    Remove /example

--- main.py
+++ main.py
@@ -10,10 +10,7 @@ app.add_url_rule('/',           view_func=default.home)
 app.add_url_rule('/robots.txt', view_func=default.robots)
 app.add_url_rule('/edit',       view_func=default.editor)
 
-@app.route('/example')
-def give_example():
-    e = render_template('pages/example.html')
-    return e
+
 
 if __name__ == '__main__':
     if not PRODUCTION:
